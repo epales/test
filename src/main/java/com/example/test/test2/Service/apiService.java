1350a60db7ebebf5c8bc71d59ec201b94ecfeda7
epales <epales@naver.com>
2024-01-30 15:54:57 +0900

    스톤 세공 스크립트 추가 및 아이템 검색 제거

--- src/main/java/com/example/test/test2/Service/apiService.java
+++ src/main/java/com/example/test/test2/Service/apiService.java
@@ -21,24 +21,24 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 
-public class apiService{
+public class apiService {
 
-    public JSONArray getMarketAllItems(String LostarkApiKey, int CategoryCode) throws InterruptedException{
-        try{
+    public JSONArray getMarketAllItems(String LostarkApiKey, int CategoryCode) throws InterruptedException {
+        try {
             URL url = new URL("https://developer-lostark.game.onstove.com/markets/items/");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("authorization", "Bearer "+LostarkApiKey);
-            httpURLConnection.setRequestProperty("accept","application/json");
-            httpURLConnection.setRequestProperty("content-Type","application/json");
+            httpURLConnection.setRequestProperty("authorization", "Bearer " + LostarkApiKey);
+            httpURLConnection.setRequestProperty("accept", "application/json");
+            httpURLConnection.setRequestProperty("content-Type", "application/json");
             httpURLConnection.setDoOutput(true);
-    
-			String parameter = "{\n"
-            + "  \"Sort\": \"CURRENT_MIN_PRICE\",\n"
-            + "  \"CategoryCode\": "+CategoryCode+",\n"
-            + "  \"PageNo\": 1,\n"
-            + "  \"SortCondition\": \"DESC\"\n"
-            + "}";
+
+            String parameter = "{\n"
+                    + "  \"Sort\": \"CURRENT_MIN_PRICE\",\n"
+                    + "  \"CategoryCode\": " + CategoryCode + ",\n"
+                    + "  \"PageNo\": 1,\n"
+                    + "  \"SortCondition\": \"DESC\"\n"
+                    + "}";
 
             byte[] out = parameter.getBytes(StandardCharsets.UTF_8);
 
@@ -48,68 +48,69 @@ public class apiService{
             int result = httpURLConnection.getResponseCode();
 
             InputStream inputStream;
-            
-            if(result == 200) {
+
+            if (result == 200) {
                 inputStream = httpURLConnection.getInputStream();
             } else {
                 inputStream = httpURLConnection.getErrorStream();
             }
-            if(inputStream == null){
+            if (inputStream == null) {
                 return null;
             }
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
             JSONParser parser = new JSONParser();
             JSONObject object = (JSONObject) parser.parse(inputStreamReader);
-            
+
             int pageNo = 0;
-			int pageSize = 10;
-			int totalCount = Integer.parseInt(object.get("TotalCount").toString());
+            int pageSize = 10;
+            int totalCount = Integer.parseInt(object.get("TotalCount").toString());
 
-			JSONArray jsonArray = new JSONArray();
+            JSONArray jsonArray = new JSONArray();
 
-			while (pageNo*pageSize < totalCount) {
-				pageNo++;
-				JSONObject data = getMarketOnePageItems(LostarkApiKey, CategoryCode, pageNo);
-				JSONArray resultJsonArray = (JSONArray) data.get("Items");
-				JSONObject tempJson = new JSONObject();
-				for(int i = 0; i < resultJsonArray.size(); i++) {
-					tempJson = (JSONObject) resultJsonArray.get(i);
-					jsonArray.add(tempJson);
-				}
-                System.out.println("\n" + pageNo +"\n");
-                Thread.sleep(500);
-			}
-            
-			httpURLConnection.disconnect();
+            while (pageNo * pageSize < totalCount) {
+                pageNo++;
+                JSONObject data = getMarketOnePageItems(LostarkApiKey, CategoryCode, pageNo);
+                JSONArray resultJsonArray = (JSONArray) data.get("Items");
+                JSONObject tempJson = new JSONObject();
+                for (int i = 0; i < resultJsonArray.size(); i++) {
+                    tempJson = (JSONObject) resultJsonArray.get(i);
+                    jsonArray.add(tempJson);
+                }
+                System.out.println("\n" + pageNo + "\n");
+                Thread.sleep(700);
+            }
 
-			return jsonArray;
+            httpURLConnection.disconnect();
+
+            return jsonArray;
         } catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (ProtocolException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
+            throw new RuntimeException(e);
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public JSONObject getMarketOnePageItems(String LostarkApiKey, int CategoryCode, int pageNo){
-        try{
+
+    public JSONObject getMarketOnePageItems(String LostarkApiKey, int CategoryCode, int pageNo) {
+        try {
             URL url = new URL("https://developer-lostark.game.onstove.com/markets/items/");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); // ���� ����
             httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("authorization", "Bearer "+LostarkApiKey);
-            httpURLConnection.setRequestProperty("accept","application/json");
-            httpURLConnection.setRequestProperty("content-Type","application/json");
+            httpURLConnection.setRequestProperty("authorization", "Bearer " + LostarkApiKey);
+            httpURLConnection.setRequestProperty("accept", "application/json");
+            httpURLConnection.setRequestProperty("content-Type", "application/json");
             httpURLConnection.setDoOutput(true);
 
             String parameter = "{\n"
-            + "  \"Sort\": \"CURRENT_MIN_PRICE\",\n"
-            + "  \"CategoryCode\": "+CategoryCode+",\n"
-            + "  \"PageNo\": "+pageNo+",\n"
-            + "  \"SortCondition\": \"DESC\"\n"
-            + "}";
-            
+                    + "  \"Sort\": \"CURRENT_MIN_PRICE\",\n"
+                    + "  \"CategoryCode\": " + CategoryCode + ",\n"
+                    + "  \"PageNo\": " + pageNo + ",\n"
+                    + "  \"SortCondition\": \"DESC\"\n"
+                    + "}";
+
             byte[] out = parameter.getBytes(StandardCharsets.UTF_8);
 
             OutputStream stream = httpURLConnection.getOutputStream();
@@ -118,7 +119,7 @@ public class apiService{
             int result = httpURLConnection.getResponseCode();
 
             InputStream inputStream;
-            if(result == 200) {
+            if (result == 200) {
                 inputStream = httpURLConnection.getInputStream();
             } else {
                 inputStream = httpURLConnection.getErrorStream();
@@ -131,44 +132,44 @@ public class apiService{
 
             return object;
         } catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (ProtocolException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
+            throw new RuntimeException(e);
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public JSONObject GetMarketsOptions(String LostarkApiKey) {
-		try {
-			URL url = new URL("https://developer-lostark.game.onstove.com/markets/options");
-			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); // ���� ����
-			httpURLConnection.setRequestMethod("GET");
-			httpURLConnection.setRequestProperty("authorization", "Bearer "+ LostarkApiKey);
-			httpURLConnection.setRequestProperty("accept","application/json");
-			httpURLConnection.setRequestProperty("content-Type","application/json");
-			httpURLConnection.setDoOutput(true);
-			int result = httpURLConnection.getResponseCode();
+        try {
+            URL url = new URL("https://developer-lostark.game.onstove.com/markets/options");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); // ���� ����
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("authorization", "Bearer " + LostarkApiKey);
+            httpURLConnection.setRequestProperty("accept", "application/json");
+            httpURLConnection.setRequestProperty("content-Type", "application/json");
+            httpURLConnection.setDoOutput(true);
+            int result = httpURLConnection.getResponseCode();
 
-			InputStream inputStream;
-			if(result == 200) {
-				inputStream = httpURLConnection.getInputStream();
-			} else {
-				inputStream = httpURLConnection.getErrorStream();
-			}
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            InputStream inputStream;
+            if (result == 200) {
+                inputStream = httpURLConnection.getInputStream();
+            } else {
+                inputStream = httpURLConnection.getErrorStream();
+            }
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
 
-			JSONParser parser = new JSONParser();
-			JSONObject object = (JSONObject) parser.parse(inputStreamReader);
-			httpURLConnection.disconnect();
+            JSONParser parser = new JSONParser();
+            JSONObject object = (JSONObject) parser.parse(inputStreamReader);
+            httpURLConnection.disconnect();
 
-			return object;
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (IOException | ParseException e) {
-			throw new RuntimeException(e);
-		}
-	}
+            return object;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
