1ec98836999299c4ac034a009a6978652a3258b7
epales <epales@naver.com>
2024-03-11 23:47:41 +0900

    UPDATE_0.0.7

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -20,31 +20,37 @@ input1.addEventListener('focus', () => {
     b.style.top = '28px';
     b.style.display = "block";
     bool1 = true;
+    searchAllAbil();
 })
 input2.addEventListener('focus', () => {
     b.style.top = '60px';
     b.style.display = "block";
     bool2 = true;
+    searchAllAbil();
 })
 input3.addEventListener('focus', () => {
     b.style.top = '92px';
     b.style.display = "block";
     bool3 = true;
+    searchAllAbil();
 })
 input4.addEventListener('focus', () => {
     b.style.top = '124px';
     b.style.display = "block";
     bool4 = true;
+    searchAllAbil();
 })
 input5.addEventListener('focus', () => {
     b.style.top = '156px';
     b.style.display = "block";
     bool5 = true;
+    searchAllAbil();
 })
 input6.addEventListener('focus', () => {
     b.style.top = '188px';
     b.style.display = "block";
     bool6 = true;
+    searchAllAbil();
 })
 
 input1.addEventListener('blur', () => {
@@ -72,38 +78,49 @@ input6.addEventListener('blur', () => {
     bool6 = false;
 })
 
-
-$('li').mousedown(function () {
-    if (bool1) {
-        input1.value = this.textContent;
-        bool1 = false;
-    } else if (bool2) {
-        input2.value = this.textContent;
-        bool2 = false;
-    } else if (bool3) {
-        input3.value = this.textContent;
-        bool3 = false;
-    } else if (bool4) {
-        input4.value = this.textContent;
-        bool4 = false;
-    } else if (bool5) {
-        input5.value = this.textContent;
-        bool5 = false;
-    } else if (bool6) {
-        input6.value = this.textContent;
-        bool6 = false;
-    }
-    b.style.display = "none";
-})
 input1.addEventListener('input', () => {
     
     if (b.hasChildNodes()) {
         b.replaceChildren();
     }
-
     searchAbil(input1.value);
-
 })
+input2.addEventListener('input', () => {
+    
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    searchAbil(input2.value);
+})
+input3.addEventListener('input', () => {
+    
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    searchAbil(input3.value);
+})
+input4.addEventListener('input', () => {
+    
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    searchAbil(input4.value);
+})
+input5.addEventListener('input', () => {
+    
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    searchAbil(input5.value);
+})
+input6.addEventListener('input', () => {
+    
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    searchAbil(input6.value);
+})
+
 function searchAbil(word) {
     $.ajax({
         url: '/access/list/' + word,
@@ -119,10 +136,52 @@ function searchAbil(word) {
         }
     });
 }
+function searchAllAbil() {
+    if (b.hasChildNodes()) {
+        b.replaceChildren();
+    }
+    
+    $.ajax({
+        url: '/access/list',
+        type: "GET",
+        dataType: 'json',
+        data: { name: name }	// 검색 키워드
+    }).done(function (result) { 	// 성공
+        if (result.length == 0) {
+          return;
+        }
+        for (var i = 0; i < result.length; i++){
+            searchAbil1(result[i]);
+        }
+    });
+}
+
 function searchAbil1(result) { 
     var li = document.createElement('li');
 
-    console.log(result);
+    $('li').mousedown(function () {
+        if (bool1) {
+            input1.value = this.textContent;
+            bool1 = false;
+        } else if (bool2) {
+            input2.value = this.textContent;
+            bool2 = false;
+        } else if (bool3) {
+            input3.value = this.textContent;
+            bool3 = false;
+        } else if (bool4) {
+            input4.value = this.textContent;
+            bool4 = false;
+        } else if (bool5) {
+            input5.value = this.textContent;
+            bool5 = false;
+        } else if (bool6) {
+            input6.value = this.textContent;
+            bool6 = false;
+        }
+        b.style.display = "none";
+    })
+
     li.textContent = result.name;
     li.role = 'option';
     b.appendChild(li);
